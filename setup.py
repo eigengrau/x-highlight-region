73f1ea20a50d0843527309b1b75aa8c526a73876
Sebastian Reuße <seb@wirrsal.net>
2019-09-21 09:25:36 +0200

    Bump pygobject dependency

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ],
     package_dir={'': 'src'},
     install_requires=[
-        'pygobject >= 3.18, < 3.29',
+        'pygobject >= 3.18, < 3.33',
         'python3-keybinder >= 1.1, < 1.2'
     ],
     license="GPL3",
